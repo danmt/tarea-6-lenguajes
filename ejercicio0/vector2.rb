d0ad8e9c142fe5078e6a08ad39ad4db5edd4acd7
Daniel Marin <danielarturomt@gmail.com>
2021-03-03 16:16:19 -0300

    feat(ejercicio0): agregar metodo magnitud y to_s

--- ejercicio0/vector2.rb
+++ ejercicio0/vector2.rb
@@ -4,5 +4,12 @@ class Vector2
   def initialize(x, y)
     @x, @y = x, y
   end
-end
 
+  def magnitud() 
+    return Math.sqrt((@x ** 2) + (@y ** 2))
+  end
+
+  def to_s()
+    return "(#{@x},#{@y})"
+  end
+end
