8723b4beadefb5c86d49b45b4becd87931217427
Daniel Marin <danielarturomt@gmail.com>
2021-03-04 12:04:00 -0300

    feat(ejercicio0): agregar complejos con composicion

--- ejercicio0.rb
+++ ejercicio0.rb
@@ -76,3 +76,67 @@ class Complejo < Vector2
     end
   end
 end
+
+class Complejo2
+  attr_accessor :x, :y
+
+  def initialize(x, y = 0)
+    @x, @y = x, y
+  end
+
+  def to_s()
+    if (@x == 0) 
+      return "#{@y}i"
+    elsif (@y == 0) 
+      return "#{@x}"
+    elsif (@y > 0)
+      return "#{@x}+#{@y}i"
+    else
+      return "#{@x}#{@y}i"
+    end
+  end
+
+  def *(complejo)
+    return Complejo.new(
+      (@x * complejo.x) - (@y * complejo.y),   
+      (@x * complejo.y) + (@y * complejo.x)
+    )
+  end
+
+  def ~@
+    return Complejo.new(@x, -@y)
+  end
+
+  def /(complejo) 
+    begin
+      if (complejo.x == 0.0) && (complejo.y == 0.0)
+        raise ZeroDivisionError end
+
+      return Complejo.new(
+        (@x * complejo.x + @y * complejo.y) / (complejo.x ** 2 + complejo.y ** 2),   
+        (@y * complejo.x - @x * complejo.y) / (complejo.x ** 2 + complejo.y ** 2)
+      )
+    rescue ZeroDivisionError
+      puts "No se puede dividir entre 0"
+    end
+  end
+end
+
+
+complejo1 = Complejo.new(3,2) * Complejo.new(1,7)
+complejo2 = Complejo.new(3,2) / Complejo.new(0);
+complejo3 = Complejo.new(3.0,2.0) / Complejo.new(1.0,-2.0);
+complejo4 = Complejo.new(3,2) / Complejo.new(0.0);
+
+puts complejo1
+puts ~complejo1
+puts complejo3
+
+complejo1 = Complejo2.new(3,2) * Complejo2.new(1,7)
+complejo2 = Complejo2.new(3,2) / Complejo2.new(0);
+complejo3 = Complejo2.new(3.0,2.0) / Complejo2.new(1.0,-2.0);
+complejo4 = Complejo2.new(3,2) / Complejo2.new(0.0);
+
+puts complejo1
+puts ~complejo1
+puts complejo3
