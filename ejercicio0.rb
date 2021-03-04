d1dfee05724206174a31ab6beac16d91be0bf85b
Daniel Marin <danielarturomt@gmail.com>
2021-03-04 11:36:01 -0300

    feat(ejercicio0): agregar metodos aritmeticos a complejos

--- ejercicio0.rb
+++ ejercicio0.rb
@@ -50,4 +50,29 @@ class Complejo < Vector2
       return "#{@x}#{@y}i"
     end
   end
+
+  def *(complejo)
+    return Complejo.new(
+      (@x * complejo.x) - (@y * complejo.y),   
+      (@x * complejo.y) + (@y * complejo.x)
+    )
+  end
+
+  def ~@
+    return Complejo.new(@x, -@y)
+  end
+
+  def /(complejo) 
+    begin
+      if (complejo.x == 0.0) && (complejo.y == 0.0)
+        raise ZeroDivisionError end
+
+      return Complejo.new(
+        (@x * complejo.x + @y * complejo.y) / (complejo.x ** 2 + complejo.y ** 2),   
+        (@y * complejo.x - @x * complejo.y) / (complejo.x ** 2 + complejo.y ** 2)
+      )
+    rescue ZeroDivisionError
+      puts "No se puede dividir entre 0"
+    end
+  end
 end
